25f9c0b18462376cd778023b019725bc65f1e84d
Mark Kimsal <metrofindings@gmail.com>
2011-10-29 17:32:14 -0400

    Silence potential errors.

--- src/whv/dashboard.php
+++ src/whv/dashboard.php
@@ -12,6 +12,13 @@ class Cgn_Service_Whv_Dashboard extends Cgn_Service {
 		$t['profile'] = $account->_dataItem->valuesAsArray();
 
 		$address = Account_Address::loadByAccountId($account->_dataItem->getPrimaryKey());
+		//db errors are "trigger_errors" in case the Cgn_ErrorStack is not used
+		// as a handler.
+		// an upgrade to the cgn_account_attrib table may result in an
+		// error as tables are only dynamically rebuilt on insert/update
+//		$e = Cgn_ErrorStack::pullError('php');
+
+
 		$t['profile'] = array_merge($address->valuesAsArray(), $t['profile']);
 		$t['profile'] = array_merge($t['profile'], $account->attributes);
 
